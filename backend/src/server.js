c5df2bb8e86f4bc3493e66c8a95f9c4723017789
xuantien0201 <xtien1812@gmail.com>
2025-11-04 17:47:17 +0700

    change url match be fe for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,30 @@ import taskRoute from './routes/tasksRoutes.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 //middleware
 app.use(express.json());
 
-app.use(cors ({ origin: "http://localhost:5173"}));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use("/api/tasks", taskRoute)
+}
 
+app.use("/api/tasks", taskRoute);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  })
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
